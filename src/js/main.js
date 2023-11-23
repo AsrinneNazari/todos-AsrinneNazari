bc8ec01e1b1dc572b8211eaf0bb815e5a5ac2c62
Asrinne Nazari <asse_89@hotmail.com>
2023-11-23 13:45:46 +0100

    last commit maybe

--- src/js/main.js
+++ src/js/main.js
@@ -1,101 +1,91 @@
 import "./../scss/style.scss";
 import { Tasks } from "./tasks";
-window.onload = () =>{ 
+window.onload = () => {
+  const a = new Tasks("Städa");
+  const c = new Tasks("Tvätta");
+  const b = new Tasks("Handla");
+  const d = new Tasks("Gymma");
+  const todoList = [a, b, c, d];
+  const doneToDos = [];
 
-const a = new Tasks ("Gymma");
-const c = new Tasks ("Tvätta");
-const b = new Tasks ("Handla");
-const d = new Tasks ("Städa");
-const todoList = [a, b, c, d];
-const doneToDos = [];
+  const sectionContList = document.getElementById("sectionContList");
+  const ulList = document.getElementById("ul-list");
 
-const sectionContList = document.getElementById("sectionContList"); 
-const ulList = document.getElementById("ul-list");
+ 
+  function makeListsWork() {
+    ulList.innerHTML = "";
+    todoList.forEach((task, e) => {
+      const liList = document.createElement("li");
+      liList.className = "li-list";
+      liList.innerText = task.name;
+      localStorage.setItem("todo", JSON.stringify(todoList));
+      liList.addEventListener("click", () => {
+        doneToDos.push(task);
+        
+        todoList.splice(e, 1);
 
-/* Local storage */
-function localStorage (){
-const storedTodoList = JSON.parse(localStorage.getItem("todo"));
-localStorage.setItem("todo", JSON.stringify(todoList));
-}
+        makeListsWork();
+        createHTMLdoneToDos();
+      });
 
-function makeListsWork (){
-ulList.innerHTML ="";
-todoList.forEach((task, e) => {
-const liList = document.createElement("li");    
-liList.className ="li-list";
-liList.innerText = task.name;
+      sectionContList.appendChild(ulList);
+      ulList.appendChild(liList);
+      liList.innerHTML = task.name;
+    });
+  }
 
-liList.addEventListener("click", () =>{  
-    doneToDos.push(task);           
-    todoList.splice(e, 1); 
+  const sectiondoneToDos = document.getElementById("section-doneToDos");
+  const createHTMLdoneToDos = () => {
+    sectiondoneToDos.innerHTML = "";
+    doneToDos.forEach((tasked, i) => {
+      const doneStuffsection = document.createElement("section");
+      doneStuffsection.id = "doneStuffsection";
+      doneStuffsection.innerHTML = tasked.name;
 
-    makeListsWork ();
-    createHTMLdoneToDos(); 
-    localStorage ();
-})
+      doneStuffsection.addEventListener("click", () => {
+        doneToDos.splice(i, 1);
+        createHTMLdoneToDos();
+      });
+      sectiondoneToDos.appendChild(doneStuffsection);
+    });
+  };
 
-sectionContList.appendChild(ulList);
-ulList.appendChild(liList);
-liList.innerHTML=task.name;
-}); 
-}
-const sectiondoneToDos = document.getElementById("section-doneToDos");
-const createHTMLdoneToDos = () =>{
-sectiondoneToDos.innerHTML =""; 
+  addNewtoDo();
+  makeListsWork();
+  sortIt();
 
-doneToDos.forEach((tasked, i) => {
-const doneStuffsection = document.createElement("section");
-doneStuffsection.id = "doneStuffsection";
-doneStuffsection.innerHTML = tasked.name;    
-
-doneStuffsection.addEventListener("click", () => {
-    doneToDos.splice(i, 1); 
-    createHTMLdoneToDos();
-  });
-  sectiondoneToDos.appendChild(doneStuffsection);
-});
-};
-
-addNewtoDo();
-makeListsWork();
-
-/* Sortering */
-
-function sortIt (){
+  /* Sortering */
+  function sortIt() {
     const sort = document.getElementById("sort-icon");
-    sort.addEventListener("click", (e) => {
-        todoList.sort(((a, b) => a - b));
-    }
-    )
+    sort.addEventListener("click", () => {
+      todoList.sort((a, b) => a.name.localeCompare(b.name));
+      makeListsWork();
+    });
+  }
 
-}
-
-/* Form */
-function addNewtoDo(){
-    
+  /* Form */
+  function addNewtoDo() {
     const form = document.getElementById("form");
     const input = document.getElementById("input");
-    
-    form.addEventListener("submit", (e) => {
-        e.preventDefault();
-     
-        const newTodo = input.value; 
-        const newTask = new Tasks (newTodo);
-        todoList.push(newTask);
-        makeListsWork ();
 
-    doneToDos.addEventListener("click", (e) => {    
+    form.addEventListener("submit", (e) => {
+      e.preventDefault();
+
+      const newTodo = input.value;
+      const newTask = new Tasks(newTodo);
+      todoList.push(newTask);
+      makeListsWork();
+
+      doneToDos.addEventListener("click", (e) => {
         newTodo.splice(e, 1);
-        
+
         const liList2 = document.createElement("li");
         liList2.className = "li-list";
         liList2.innerHTML = newTodo;
         ulList.appendChild(liList2);
         input.value = "";
-    })
-    createHTMLdoneToDos();
-    localStorage();
-    
-});
-}
-}
+      });
+      createHTMLdoneToDos();
+    });
+  }
+};
