89c3369682ccf7d714f2ba67f034d879decdcaf4
Asrinne Nazari <asse_89@hotmail.com>
2024-01-30 13:40:15 +0100

    changing stuff

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/todo-AsrinneNazari',
+    base: '/todos-AsrinneNazari',
     plugins: [vue()]
 })
