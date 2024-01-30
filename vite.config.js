d50fd926dc586b74cd634b0dd2691e4b4f1bf3c8
Asrinne Nazari <asse_89@hotmail.com>
2024-01-30 14:14:18 +0100

    a

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,3 @@
-/* import { defineConfig } from "vite";
-import vue from '@vitejs/plugin-vue'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: '/todos-AsrinneNazari',
-    plugins: [vue()]
-}) */
-
 import { defineConfig } from "vite";
 
 export default defineConfig({
